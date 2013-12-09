8e91b000e26249e0c2d354494495971c5cc6b1f7
Phil Deisinger <phil.deisinger@shoutlet.com>
2013-12-09 12:38:50 -0600

    some js stuff

---
+++ js/home.js
@@ -0,0 +1,7 @@
+<script type="text/javascript">
+$(document).ready(function(){
+	$(document.body).on("click", ".magneto", function(e) {
+		window.location="http://www.google.com";
+	});
+});
+</script>
